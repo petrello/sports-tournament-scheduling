d3b8d43bf27e1de3f14b4934dfe9ecf8a9583aef
petrello <tommasop01@gmail.com>
2025-07-26 19:18:05 +0200

    doc: add doc-string to sat encodings

--- source/SAT/sat_encodings.py
+++ source/SAT/sat_encodings.py
@@ -4,12 +4,15 @@ from z3 import z3
 
 
 def exactly_one(bs: List[z3.BoolRef]) -> z3.BoolRef:
+    """Exactly one variable is True."""
     return z3.And(z3.Or(bs), z3.AtMost(*bs, 1))
 
 def at_most_k(bs: List[z3.BoolRef], k: int) -> z3.BoolRef:
+    """At most k Booleans are True."""
     return z3.AtMost(*bs, k)
 
 def equiv(a: z3.BoolRef, b: z3.BoolRef) -> z3.BoolRef:
+    """Bi-implication."""
     return z3.And(z3.Implies(a,b), z3.Implies(b,a))
 
 def less_or_equal_onehot(a: List[z3.BoolRef], b: List[z3.BoolRef]) -> z3.BoolRef:
