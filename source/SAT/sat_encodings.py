f00f54de83187f86b9253c4c411d56933a864809
petrello <tommasop01@gmail.com>
2025-07-27 12:54:34 +0200

    fix: encodings support SAT and DIMACS
    - refactor of at_most_one
    - refactor of at_most_k

--- source/SAT/sat_encodings.py
+++ source/SAT/sat_encodings.py
@@ -1,15 +1,41 @@
 from typing import List
 
-from z3 import z3
+from z3 import z3, Bool, Or, Not, And
 
 
-def exactly_one(bs: List[z3.BoolRef]) -> z3.BoolRef:
-    """Exactly one variable is True."""
-    return z3.And(z3.Or(bs), z3.AtMost(*bs, 1))
+def at_least_one(bs: List[z3.BoolRef]) -> z3.BoolRef:
+    """At least one variable is True."""
+    return z3.Or(bs)
 
-def at_most_k(bs: List[z3.BoolRef], k: int) -> z3.BoolRef:
-    """At most k Booleans are True."""
-    return z3.AtMost(*bs, k)
+def at_most_one(bs: List[z3.BoolRef]) -> List[z3.BoolRef]:
+    return [
+        z3.Or(z3.Not(bs[i]), z3.Not(bs[j]))
+        for i in range(len(bs))
+        for j in range(i+1, len(bs))
+    ]
+
+def exactly_one(bs: list[z3.BoolRef]) -> z3.BoolRef:
+    return z3.And([at_least_one(bs), *at_most_one(bs)])
+
+
+def at_most_k(bool_vars, k):
+    constraints = []
+    n = len(bool_vars)
+    s = [[Bool(f"s_{i}_{j}") for j in range(k)] for i in range(n - 1)]
+    constraints.append(Or(Not(bool_vars[0]), s[0][0]))
+    constraints += [Not(s[0][j]) for j in range(1, k)]
+
+    for i in range(1, n - 1):
+        constraints.append(Or(Not(bool_vars[i]), s[i][0]))
+        constraints.append(Or(Not(s[i - 1][0]), s[i][0]))
+        constraints.append(Or(Not(bool_vars[i]), Not(s[i - 1][k - 1])))
+        for j in range(1, k):
+            constraints.append(Or(Not(bool_vars[i]), Not(s[i - 1][j - 1]), s[i][j]))
+            constraints.append(Or(Not(s[i - 1][j]), s[i][j]))
+
+    constraints.append(Or(Not(bool_vars[n - 1]), Not(s[n - 2][k - 1])))
+
+    return And(constraints)
 
 def equiv(a: z3.BoolRef, b: z3.BoolRef) -> z3.BoolRef:
     """Bi-implication."""
